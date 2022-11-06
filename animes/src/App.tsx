3a561e2a3955798d13cf4aa569baa1a61fbada3a
TomoriComAncora <lucas_fernando@ufms.br>
2022-11-05 20:01:38 -0400

    pesquisa

--- animes/src/App.tsx
+++ animes/src/App.tsx
@@ -1,22 +1,29 @@
 import { useEffect, useState } from 'react'
 
 function App() {
- 
+  
+  const [pesquisa, setPesquisa] = useState('Naruto');
   const [dadosAnime, setDadosAnime] = useState();
 
-  const getData = async() =>{
-    const resp = await fetch(`https://api.jikan.moe/v4/anime?q=naruto&limit=21`);
+  const getData = async () => {
+    const resp = await fetch(`https://api.jikan.moe/v4/anime?q=${pesquisa}&limit=21`);
     const respData = await resp.json();
     setDadosAnime(respData.data);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[]);
+  }, [pesquisa]);
 
   return (
     <>
-      
+      <div className="cabecalho">
+        <h1>Minha Lista de Animes</h1>
+        <div className="pesquisa">
+          <input type="search" placeholder='Buscar...' 
+          onChange={(e)=>setPesquisa(e.target.value)}/>
+        </div>
+      </div>
     </>
   )
 }
